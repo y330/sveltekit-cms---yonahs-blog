3aeddbfd3a623d63ec83ac7dd4defcf4ac9b8d90
y330 <avivyonah@gmail.com>
2021-06-24 10:48:29 -0400

    done :)

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,20 +1,25 @@
 <script>
-import Me from "$lib/me.svelte";
+	import Me from "$lib/me.animation.svelte";
 </script>
+
+<div
+	class="hero min-h-[90vh]"
+	style="background-image: url(`https://media-exp1.licdn.com/dms/image/C4D16AQFrm5IAV17UWw/profile-displaybackgroundimage-shrink_350_1400/0/1621482177981?e=1629936000&v=beta&t=SmBnv8jcEIrP_cHUdelgtes0faTCkgLzZnzZnPvIH5Q`
+);"
+>			<Me />
+
+	<div class="hero-overlay bg-opacity-60" />
+	<div class="text-center hero-content text-neutral-content">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">My English FST - ENG3U</h1>
+			<p class="mb-5">
+				Blog about what I learned in english this year, focusing on the theme of deception in dystopia in the literature
+				we studied
+			</p>
+		</div>
+	</div>
+</div>
 
 <style>
 	/* your styles go here */
 </style>
-
-<div class="hero min-h-[90vh]" style='background-image: url("https://media-exp1.licdn.com/dms/image/C4D16AQFrm5IAV17UWw/profile-displaybackgroundimage-shrink_350_1400/0/1621482177981?e=1629936000&v=beta&t=SmBnv8jcEIrP_cHUdelgtes0faTCkgLzZnzZnPvIH5Q
-");'>
-	<div class="hero-overlay bg-opacity-60" />
-	<div class="text-center hero-content text-neutral-content">
-	  <div class="max-w-md">
-		<h1 class="mb-5 text-5xl font-bold">My English FST - ENG3U</h1>
-		<p class="mb-5">Blog about what I learned in english this year, focusing on the theme of deception in dystopia in the literature we studied</p>
-		<Me/>
-
-	  </div>
-	</div>
-  </div>
